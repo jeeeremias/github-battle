7c4cab78be78080b2260918bea2feb358e456ccf
Jeremias Pereira <jeremias.livre@gmail.com>
2017-02-25 15:10:57 -0300

    Created new components and using nested components to pass props

--- app/index.js
+++ app/index.js
@@ -1,17 +1,61 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+var USER_DATA = {
+    name: 'Jeremias Pereira',
+    username: 'jeeeremias',
+    image: 'https://avatars0.githubusercontent.com/u/9812493?v=3&s=460'
+}
 
-var HelloWorld = React.createClass({
-    render: function () {
+import React from 'react';
+import ReactDOM from'react-dom';
+
+class HelloWorld extends React.Component {
+    render () {
         return (
             <div>
-                Hello world!
+                Hello {this.props.name}!
             </div>
         )
     }
-});
+};
+
+class ProfilePic extends React.Component {
+    render () {
+        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    }
+};
+
+class ProfileLink extends React.Component {
+    render () {
+        return (
+            <div>
+                <a href={'https://www.github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
+        )
+    }
+};
+
+class ProfileName extends React.Component {
+    render () {
+        return (
+            <div>{this.props.name}</div>
+        )
+    }
+};
+
+class Avatar extends React.Component {
+    render () {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-    <HelloWorld />,
+    <Avatar user={USER_DATA} />,
     document.getElementById('app')
 )
