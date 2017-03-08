42573631f69e821e7405d5160abf86ace18c0014
Jeremias Pereira <jeremias.livre@gmail.com>
2017-03-08 20:47:02 -0300

    Created component with input and some bootstrap classes

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,11 +3,14 @@ import {Route, Router, IndexRoute, hashHistory} from 'react-router'
 
 import Main from '../components/Main'
 import Home from '../components/Home'
+import PromptComponent from '../containers/PromptContainer'
 
 const routes = (
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
             <IndexRoute component={Home}/>
+            <Route path="playerOne" header="Player One" component={PromptComponent}/>
+            <Route path="playerTwo/:playerOne" header="Player One" component={PromptComponent}/>
         </Route>
     </Router>
 );
---
+++ app/containers/PromptContainer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const PromptContainer = ({route}) => (
+    <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+        <h1>{route.header}</h1>
+        <div className="col-sm-12">
+            <form>
+                <div className="form-group">
+                    <input
+                        className="form-control"
+                        placeholder="GitHub Username"
+                        type="text"/>
+                </div>
+                <div className="form-group col-sm-4 col-sm-offset-4">
+                    <button
+                        className="btn btn-block btn-success"
+                        type="submit">
+                        Continue
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+);
+
+export default PromptContainer;
