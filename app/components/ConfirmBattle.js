cec0f71176e31446e96a4b5c6fddf177df4807d2
Jeremias Pereira <jeremias.livre@gmail.com>
2017-03-15 22:15:37 -0300

    New ConfirmBattle component

---
+++ app/components/ConfirmBattle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ConfirmBattle = (props) => {
+    return (
+        <div>Confirm battle!</div>
+    )
+};
+
+export default ConfirmBattle;
---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ConfirmBattleContainer extends React.Component {
+
+    render () {
+        return (
+            <div/>
+        )
+    }
+};
+
+export default ConfirmBattleContainer;
