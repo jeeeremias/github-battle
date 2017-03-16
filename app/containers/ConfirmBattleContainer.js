0b881281824c601b590be6e5bdf235219bafa593
Jeremias Pereira <jeremias.livre@gmail.com>
2017-03-15 22:25:16 -0300

    put ConfirmBattle container in routes and made work

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+
+import ConfirmBattle from '../components/ConfirmBattle'
 
 class ConfirmBattleContainer extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            playerInfo: []
+        };
+    }
 
     render () {
         return (
-            <div/>
+            <ConfirmBattle/>
         )
     }
+};
+
+ConfirmBattleContainer.contextTypes = {
+    router: React.PropTypes.object.isRequired
 };
 
 export default ConfirmBattleContainer;
